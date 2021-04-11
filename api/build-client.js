28412c0195f302d67610ffe8ddf7539c221c5ab9
sz <shalominfinity@gmail.com>
2021-04-11 19:52:16 +0300

    m

--- api/build-client.js
+++ api/build-client.js
@@ -8,12 +8,12 @@ export default ({ req }) => {
       headers: req.headers
     });
   } else {
-    if (window.location.protocol.indexOf('https') == 0){
-      var el = document.createElement('meta')
-      el.setAttribute('http-equiv', 'Content-Security-Policy')
-      el.setAttribute('content', 'upgrade-insecure-requests')
-      document.head.append(el)
-    }
+    // if (window.location.protocol.indexOf('https') == 0){
+    //   var el = document.createElement('meta')
+    //   el.setAttribute('http-equiv', 'Content-Security-Policy')
+    //   el.setAttribute('content', 'upgrade-insecure-requests')
+    //   document.head.append(el)
+    // }
     // We must be on the browser
     return axios.create({
       baseURL: 'https://www.ticket-app-sz.website',
